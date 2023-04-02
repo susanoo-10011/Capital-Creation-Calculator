d0e63022cd3b1ab957dfa95e4be2b4061865065e
susanoo-10011 <cycahosir@gmail.com>
2023-04-02 19:36:22 +0700

    Создал метод подсчета капитала для 1 года. Код по факту не рабочий, множество костылей.

--- Project/Project/ResultsForm.cs
+++ Project/Project/ResultsForm.cs
@@ -54,6 +54,20 @@ namespace Project
 
             expensesYear1.Text = Convert.ToString(expenses);
             Expenses(expenses, inflation);
+
+            deltaYear1.Text = Convert.ToString(income - expenses);
+            Delta(expenses, inflation, income, incomeGrowth);
+
+            accumulatedCapitalYear1.Text = Convert.ToString((income - expenses) * 12);
+            AccumulatedCapitalYear(expenses, inflation, income, incomeGrowth);
+
+            LastYearCapital();
+
+            PercentageOfLastYear(startUpCapital);
+
+            PercentageYear();
+
+            CurrentCapital1();
         }
 
         public void MonthlyIncome(double income, double incomeGrowth)
@@ -69,21 +83,115 @@ namespace Project
 
         public void Expenses(double expenses, double inflation)
         {
-            for (int i = 2;i <= 3; i++)
+            for (int i = 2; i <= 3; i++)
             {
-                expenses *= (1+ inflation / 100);
+                expenses *= (1 + inflation / 100);
                 Label expensesYear1 = Controls.Find("expensesYear" + i, true).FirstOrDefault() as Label;
                 expensesYear1.Text = Convert.ToString(expenses);
             }
         }
 
-        public ResultsForm()
+        public void Delta(double expenses, double inflation, double income, double incomeGrowth)
         {
-            InitializeComponent();
+            for (int i = 2; i <= 3; i++)
+            {
+                expenses *= (1 + inflation / 100);
+                income += income * (incomeGrowth / 100);
+
+                double delta = income - expenses;
+                Label deltaYear = Controls.Find("deltaYear" + i, true).FirstOrDefault() as Label;
+                deltaYear.Text = Convert.ToString(delta);
+            }
         }
-        private void CloseButton_Click(object sender, EventArgs e)
+
+        public void AccumulatedCapitalYear(double expenses, double inflation, double income, double incomeGrowth)
         {
-            Application.Exit();
+            for (int i = 2; i <= 3; i++)
+            {
+                expenses *= (1 + inflation / 100);
+                income += income * (incomeGrowth / 100);
+
+                double delta = (income - expenses) * 12;
+                Label accumulatedCapitalYear = Controls.Find("accumulatedCapitalYear"
+                    + i, true).FirstOrDefault() as Label;
+                accumulatedCapitalYear.Text = Convert.ToString(delta);
+            }
         }
+
+        
+        public void PercentageOfLastYear(double startUpCapital)
+        {
+            percentageOfLastYear1.Text = Convert.ToString(startUpCapital * investmentRate / 100);
+
+
+        }
+
+        public void PercentageYear()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Label accumulatedCapitalYear = Controls.Find("accumulatedCapitalYear"
+                + i, true).FirstOrDefault() as Label;
+                Label percentageYear = Controls.Find("percentageYear"
+                    + i, true).FirstOrDefault() as Label;
+                double accumulatedCapital = double.Parse(accumulatedCapitalYear.Text);
+                double result = accumulatedCapital * investmentRate / 100 / 2;
+                percentageYear.Text = Convert.ToString(result);
+            }
+        }
+
+        public void LastYearCapital()
+        {
+            lastYearCapitalYear1.Text = Convert.ToString(startUpCapital);
+
+            for (int i = 2; i <= 3; i++)
+            {
+                Label currentCapital = Controls.Find("currentCapital" + i, true).
+                    FirstOrDefault() as Label;
+                Label lastYearCapitalYear = Controls.Find("lastYearCapitalYear" + i, true).
+                    FirstOrDefault() as Label;
+                lastYearCapitalYear.Text = Convert.ToString(currentCapital);
+
+            }
+        }
+
+        double LastCapital;
+        public void CurrentCapital1()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Label accumulatedCapitalYear = Controls.Find("accumulatedCapitalYear" + i, true).
+                    FirstOrDefault() as Label;
+                double accumulatedCapital = double.Parse(accumulatedCapitalYear.Text);
+
+                Label lastYearCapitalYear = Controls.Find("lastYearCapitalYear" + i, true).
+                    FirstOrDefault() as Label;
+                lastYearCapitalYear.Text = Convert.ToString(LastCapital);
+
+
+                Label percentageOfLastYear = Controls.Find("percentageOfLastYear" + i, true).
+                    FirstOrDefault() as Label;
+                double percentageLastYear = double.Parse(percentageOfLastYear.Text);
+
+                Label percentageYear = Controls.Find("percentageYear" + i, true).
+                    FirstOrDefault() as Label;
+                double percentageCurrentYear = double.Parse(percentageYear.Text);
+
+                double capital = accumulatedCapital + LastCapital + percentageLastYear + percentageCurrentYear;
+                Label currentCapital = Controls.Find("currentCapital" + i, true).
+                    FirstOrDefault() as Label;
+
+                LastCapital = Convert.ToDouble(capital);
+                currentCapital.Text = Convert.ToString(capital);
+            }
+        }
+            public ResultsForm()
+            {
+                InitializeComponent();
+            }
+            private void CloseButton_Click(object sender, EventArgs e)
+            {
+                Application.Exit();
+            }
     }
 }
